e6b9ad0344a4e506f464f8cfc69c6e5fb64f0f8c
Orfeas Kypris <kypriso@gmail.com>
2022-12-10 01:05:31 +0200

    refactor

--- tests/scratch.py
+++ tests/scratch.py
@@ -1,8 +1,25 @@
 import pyoniip
 import numpy as np
 import nptyping
+import time
 
 
+def st_time(func):
+    """
+        st decorator to calculate the total time of a func
+    """
+
+    def st_func(*args, **kwargs):
+        t1 = time.time()
+        r = func(*args, **kwargs)
+        t2 = time.time()
+        print("Function=%s, Time=%s" % (func.__name__, t2 - t1))
+        return r
+
+    return st_func
+
+
+@st_time
 def compute_energy(audio: nptyping.NDArray, hop_len: int, win_len: int):
     audio_len = audio.shape[1]
     energy = np.array(
@@ -10,6 +27,7 @@ def compute_energy(audio: nptyping.NDArray, hop_len: int, win_len: int):
     return energy
 
 
+@st_time
 def compute_energy_cpp(audio: nptyping.NDArray, hop_len: int, win_len: int):
     energy = pyoniip.compute_energy(audio, hop_len, win_len)
     return energy
@@ -17,15 +35,24 @@ def compute_energy_cpp(audio: nptyping.NDArray, hop_len: int, win_len: int):
 
 def main():
 
-    audio = np.array(
-        [
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        ], dtype=np.float32)
-    win_len = 5
-    hop_len = 2
+    # audio = np.array(
+    #     [
+    #         [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    #         [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    #     ], dtype=np.float32)
+
+    audio = np.ones([2, 6648320], dtype=np.float32)
+
+    win_len = 108
+    hop_len = 54
+
     print("python: {}".format(compute_energy(audio, hop_len, win_len)))
     print("cpp: {}".format(compute_energy_cpp(audio, hop_len, win_len)))
+
+    energy = compute_energy(audio, hop_len, win_len)
+    energy_cpp = compute_energy_cpp(audio, hop_len, win_len)
+    print(energy.shape)
+    print(energy_cpp.shape)
 
 
 if __name__ == '__main__':
