fix energy computation and udpate tests

tests/test_compute_energy.py
@@ -45,5 +45,5 @@ def test_compute_energy(audio: nptyping.NDArray[Shape["2, *"], Float32], win_len
     cpp = compute_energy_cpp(audio, win_len, hop_len)
     diff = np.mean(np.abs(p - cpp), axis=1)
     assert pyamtcpp.__version__ == '0.0.1'
-    assert np.mean(diff) < 1e-7
+    assert np.mean(diff) < 5e-7
 

utils/benchmarking.py
@@ -23,7 +23,7 @@ def st_time(func):
 def compute_energy(audio: nptyping.NDArray, hop_len: int, win_len: int):
     audio_len = audio.shape[1]
     energy = np.array(
-        [np.mean(audio[:, i:i + win_len], axis=1) for i in range(0, audio_len, hop_len)]).T
+        [np.mean(audio[:, i:i + win_len] ** 2, axis=1) for i in range(0, audio_len, hop_len)]).T
     return energy
 
 